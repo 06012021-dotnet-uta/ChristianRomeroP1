f30f87840e3c9e9a857a0989310e1f2cf1aa9998
ChristianRevature <christian.romero@revature.net>
2021-06-21 15:37:08 -0400

    polishes w4 hr

--- W4HackerRankChristianRomero/Program.cs
+++ W4HackerRankChristianRomero/Program.cs
@@ -2,11 +2,30 @@
 
 namespace W4HackerRankChristianRomero
 {
-    class Program
+
+
+
+     class Program
     {
-        static void Main(string[] args)
+         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //create a staircase of size N
+            /*Reqs:
+             * 1 - takes in a parameter of integer N [0,100]
+             * 2 - prints a staircase of # symbol
+             * 3- 
+            */
+
+            Stair stair = new Stair();
+
+            //Messages.ObtainConsent();
+
+            //Stair.PrintStaircase(number)
+
+            stair.GetInput();
+            
+            stair.PrintStaircase(stair.number);
+                
         }
-    }
-}
+    }//eoc
+}//eon
