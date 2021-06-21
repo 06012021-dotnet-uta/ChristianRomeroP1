7e66e07da55426d90b89cf790c03f10774a61b4c
ChristianRevature <christian.romero@revature.net>
2021-06-21 16:03:56 -0400

    fixed w4 hr error

--- W4HackerRankChristianRomero/w4 hr/Class1.cs
+++ W4HackerRankChristianRomero/w4 hr/Class1.cs
@@ -1,8 +1,12 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace W4HackerRankChristianRomero
+namespace w4_hr
 {
-          public class Stair
+    public class Stair
     {
         /*Program logic:
         1 - Take in a user integer input of N
@@ -16,8 +20,9 @@ namespace W4HackerRankChristianRomero
         public int number;
 
 
-        public  int GetInput()//string "answer" will be input that tells the program to execute staircase printing or to exit 
+        public int GetInput()//string "answer" will be input that tells the program to execute staircase printing or to exit 
         {
+            Console.WriteLine("Please input an integer value according to the number of stairs you'd like!");
             number = Int32.Parse(Console.ReadLine()); //this line makes it so whenever I instantiate a stair class, it will have a number property
             return number;
         }
@@ -28,18 +33,18 @@ namespace W4HackerRankChristianRomero
             Console.WriteLine("Here's your staircase!");
             int i;
             //ffor loop for N
-            for (i = 0; i < number; i++)
+            for (i = 1; i < number+1; i++)
             {
                 //print out N #'s
-                
+
                 //Console.WriteLine($"#*{i}");let's see how this goes
                 //first attempt didn't work - did some research on string constructor and came up with this:
-                System.Console.WriteLine(new string ('#',i)); // this will print out i number of #'s per line
+                System.Console.WriteLine(new string('#', i)); // this will print out i number of #'s per line
             }
         }
 
 
 
 
-    }
-}      
+    }//eoc
+}//eon
