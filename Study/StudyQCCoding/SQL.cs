f59bedb552eb2b97d6294e70807d02fe31a8db36
ChristianRevature <christian.romero@revature.net>
2021-06-23 11:27:28 -0400

    adds QC coding study mats and updates w4 notes

--- Study/StudyQCCoding/SQL.cs
+++ Study/StudyQCCoding/SQL.cs
@@ -8,5 +8,64 @@ namespace StudyQCCoding
 {
     class SQL
     {
+
+    }
+
+
+
+    class DDL
+    {
+        /*Contains keywords like:
+         * 1) CREATE
+         * 2) ALTER
+         * 3) DROP
+         * 4) RENAME
+         * 5) TRUNCATE
+         * 6) COMMENT
+        */
+    }
+
+
+    class DQL
+    {
+        /*Contains keywords like:
+         * 1) SELECT
+         * 2) FROM
+         * 3) WHERE
+        */
+
+        Console.WriteLine("This is a SELECT query: \n SELECT colname1,colname2 +\nFROM tablename(.database)");
+
+        Console.WriteLine("We can have cross joins: \n SELECT colname1,colname2 +\n FROM tablename(.database)+\n CROSS JOIN table.colname AS alias+\n ORDER BY colorders");
+        
+
+    }
+
+
+    class DML
+    {
+        /*Contains keywords like:
+         * 1) INSERT
+         * 2) UPDATE
+         * 3) DELETE
+         * 4) MERGE
+         * 5) CALL
+         * 6) EXPLAIN PLAN
+         * 7) LOCK TABLE
+        */
+    }
+
+
+    class DCL
+    {
+        /*Contains keywords like:
+         * 5) GRANT 
+         * 6) CONTROL
+        */
+    }
+
+    class AggregateSQL
+    {
+
     }
 }
