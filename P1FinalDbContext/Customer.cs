621f1c3bab88ac0ff65dc255022ffb85fd075b65
ChristianRevature <christian.romero@revature.net>
2021-06-24 13:36:51 -0400

    updates P1 and adds PPT

--- P1FinalDbContext/Customer.cs
+++ P1FinalDbContext/Customer.cs
@@ -19,12 +19,7 @@ namespace P1FinalDbContext
         public string Password { get; set; }
         public string Email { get; set; }
         public int Favorite { get; set; }
-        public int? Member { get; set; }
-        public int? Age { get; set; }
-        public string Mailing { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public int? Zip { get; set; }
+        public string Member { get; set; }
 
         public virtual Favorite FavoriteNavigation { get; set; }
         public virtual ICollection<Order> Orders { get; set; }
